9600aed3007279d213e2662edb731a3eb49e5d67
DanielNduati <dannduati2@gmail.com>
2022-06-27 12:24:36 +0300

    Call code in an external package

--- hello/hello.go
+++ hello/hello.go
@@ -1,10 +1,14 @@
 // Declare a main package
 package main
 
-// Import the fmt package for formating text
-import "fmt"
+// Import the fmt and rsc.io/quote package
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
 
 // The main function executes by default when you run the main package
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println(quote.Go())
 }
