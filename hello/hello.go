53a6c81b8ce423c597b5ba891abc89c2342d922d
DanielNduati <dannduati2@gmail.com>
2022-06-27 12:14:36 +0300

    Add hello go code

---
+++ hello/hello.go
@@ -0,0 +1,10 @@
+// Declare a main package
+package main
+
+// Import the fmt package for formating text
+import "fmt"
+
+// The main function executes by default when you run the main package
+func main() {
+	fmt.Println("Hello, World!")
+}
