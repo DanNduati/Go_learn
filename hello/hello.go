04c82962da3d49bc5cd96bd4c6b3ac71c4c12a9c
DanielNduati <dannduati2@gmail.com>
2022-06-27 13:48:01 +0300

    Handle error from greetings module Hello function and implement a logger
    - Configure the log package to print the command name ("greetings: "),
    without a timestamp or source file info
    - Assign both of the Hello return values, including the error, to variables.
    - Change the Hello argument to an empty string for testing
    - Look for a non-nil error value.
    - If there is an error value use the functions in the standard library's
    log package to output error information. and terminate the program

--- hello/hello.go
+++ hello/hello.go
@@ -4,6 +4,7 @@ package main
 // Import the fmt, greetings and rsc.io/quote package
 import (
 	"fmt"
+	"log"
 
 	"github.com/DanNduati/Go_learn/greetings"
 	"rsc.io/quote"
@@ -11,8 +12,19 @@ import (
 
 // The main function executes by default when you run the main package
 func main() {
+	// set properties of the predefined logger,including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number
+	log.SetPrefix("grettings: ")
+	log.SetFlags(0)
+
 	//get a greeting message and print it
-	message := greetings.Hello("Daniel")
+	message, err := greetings.Hello("")
+	if err != nil {
+		//Fatal is equivalent to Print() followed by a call to os.Exit(1).
+		log.Fatal(err)
+	}
+	// if no error was returned print the returned message to the console
 	fmt.Println(message)
 	fmt.Println(quote.Go())
 }
