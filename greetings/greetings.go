0201a6816ee101d6f19ac95d2811f0ca0b19da84
DanielNduati <dannduati2@gmail.com>
2022-06-27 13:35:02 +0300

    Return error if caller name is empty
    - Import the Go standard library `errors` package
    - Refactor Hello function to return two values a `string` and an
    `error`.
    - Check for empty name string and return an error if empty
    - Add nil(no error) as a second value for successful return.

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,11 +1,18 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting that embeds the name in the message
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	//if no name was given, return an error with a message
+	if name == "" {
+		return "", errors.New("empty name")
+	}
 	// Use the fmt package's Sprintf function to create a greeting message.
 	// Sprintf substitutes the name parameter's value in the %v format verb.
 	message := fmt.Sprintf("Hello, %v. Welcome!", name)
-	return message
+	return message, nil
 }
